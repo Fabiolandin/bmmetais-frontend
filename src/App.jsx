6f3965065196a205bcfb08f956f98ee1423d1ddc
Fabio Landin <flandin1990@gmail.com>
2026-04-12 11:53:20 -0300

    add: instalando shadcn e tailwind e configurando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { Button } from "./components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader } from "./components/ui/card"
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-red-500">Meu projeto</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-red-500">Meu projeto</h1>
+      <Button variant="destructive">Click me</Button>
+      <Card>
+        <CardContent>
+          <CardHeader>
+            Olá mundo
+          </CardHeader>
+        </CardContent>
+        <CardFooter className="flex justify-end gap-2">
+          <Button>Salvar</Button>
+          <Button>Cancelar</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
