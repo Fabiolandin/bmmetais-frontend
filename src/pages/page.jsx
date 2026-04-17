de183e9be4dac3598be2542840a5d2a2e8b43734
Fabio Landin <flandin1990@gmail.com>
2026-04-16 23:06:46 -0300

    feat:estruturando react-router e primeiras paginas

---
+++ src/pages/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/ui/button"
+
+const Teste = () => {
+    return (
+        <div>
+            <h1>Página</h1>
+            <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default Teste
