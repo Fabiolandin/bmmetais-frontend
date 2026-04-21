103e0692a609395f2ae966db404f04e0e2334c33
Fabio Landin <flandin1990@gmail.com>
2026-04-21 13:29:24 -0300

    feat:Adicionando fetch de clientes 100% funcionando

---
+++ src/fetchs/fetchCliente.tsx
@@ -0,0 +1,53 @@
+export const fetchCliente = async () => {
+    const res = await fetch("http://localhost:3000/cliente", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao buscar cliente")
+    }
+    return await res.json()
+};
+
+export const createCliente = async (nome: string, cpf: string, telefone: string, email: string) => {
+    const res = await fetch("http://localhost:3000/cliente", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, cpf, telefone, email }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao criar cliente")
+    }
+    return await res.json()
+};
+
+export const deleteClientes = async (id: number) => {
+    const res = await fetch(`http://localhost:3000/cliente/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao deletar cliente")
+    }
+    return await res.json()
+};
+
+export const editarCliente = async (id: number, nome: string, cpf: string, telefone: string, email: string) => {
+    const res = await fetch(`http://localhost:3000/cliente/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, cpf, telefone, email }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao editar cliente")
+    }
+    return await res.json()
+};
