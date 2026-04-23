a85bdfdf24b173b7baadcd9a7931a42e814626f7
Fabio Landin <flandin1990@gmail.com>
2026-04-23 10:30:56 -0300

    feat: adicionando fetch de fornecedores 100% funcionando

---
+++ src/fetchs/fetchFornecedor.tsx
@@ -0,0 +1,53 @@
+export const fetchFornecedor = async () => {
+    const res = await fetch("http://localhost:3000/fornecedor", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao buscar fornecedor")
+    }
+    return await res.json()
+};
+
+export const createFornecedor = async (nome: string, cnpj: string, telefone: string, email: string) => {
+    const res = await fetch("http://localhost:3000/fornecedor", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, cnpj, telefone, email }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao criar fornecedor")
+    }
+    return await res.json()
+};
+
+export const deleteFornecedor = async (id: number) => {
+    const res = await fetch(`http://localhost:3000/fornecedor/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao deletar fornecedor")
+    }
+    return await res.json()
+};
+
+export const editarFornecedor = async (id: number, nome: string, cnpj: string, telefone: string, email: string) => {
+    const res = await fetch(`http://localhost:3000/fornecedor/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, cnpj, telefone, email }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao editar fornecedor")
+    }
+    return await res.json()
+};
