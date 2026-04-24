a75611335c2eb1c016ab483868d1967e70594d44
Fabio Landin <flandin1990@gmail.com>
2026-04-24 10:13:37 -0300

    feat: add fetch de produtos 100% funcionando

---
+++ src/fetchs/fetchProduto.tsx
@@ -0,0 +1,63 @@
+export const fetchProduto = async () => {
+    const res = await fetch("http://localhost:3000/produtos", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao buscar produto")
+    }
+    return await res.json()
+};
+
+export const createProduto = async (nome: string, descricao: string, preco: number, categoria_produtoId: number, estoque: number,) => {
+    const res = await fetch("http://localhost:3000/produtos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, descricao, preco, categoria_produtoId, estoque }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao criar produto")
+    }
+    return await res.json()
+};
+
+export const deleteProduto = async (id: number) => {
+    console.log("Chamando deleteProduto para ID:", id)
+    const res = await fetch(`http://localhost:3000/produtos/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    
+    console.log("Status da resposta de exclusão:", res.status)
+
+    if (res.status === 204) return null
+
+    if (!res.ok) {
+        const errorMsg = await res.text()
+        console.error("Erro detalhado do servidor:", errorMsg)
+        throw new Error("Erro ao deletar produto")
+    }
+
+    const text = await res.text()
+    return text ? JSON.parse(text) : null
+};
+
+export const updateProduto = async (id: number, nome: string, descricao: string, preco: number, categoria_produtoId: number, estoque: number) => {
+    const res = await fetch(`http://localhost:3000/produtos/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nome, descricao, preco, categoria_produtoId, estoque }),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao atualizar produto")
+    }
+    return await res.json()
+};
