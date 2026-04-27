9b59b8c1d6b9d21ffc01000f382c5f70ebc333f5
Fabio Landin <flandin1990@gmail.com>
2026-04-27 16:24:33 -0300

    feat: adicionando fetch de pedidos 100% funcionando

---
+++ src/fetchs/fetchPedido.tsx
@@ -0,0 +1,40 @@
+export const fetchPedido = async () => {
+    const res = await fetch("http://localhost:3000/pedido", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao buscar pedido")
+    }
+    return await res.json()
+};
+
+export const createPedido = async (data: { clienteId: number, funcionarioId: number, items: { produtoId: number, quantidade: number, preco_unitario: number }[] }) => {
+    const res = await fetch("http://localhost:3000/pedido", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+    if (res.status !== 200 && res.status !== 201) {
+        throw new Error("Erro ao criar pedido")
+    }
+    return await res.json()
+};
+
+export const deletePedido = async (id: number) => {
+    const res = await fetch(`http://localhost:3000/pedido/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    if (res.status !== 200 && res.status !== 201 && res.status !== 204) {
+        throw new Error("Erro ao deletar pedido")
+    }
+    return res.status === 204 ? null : await res.json()
+};
+
