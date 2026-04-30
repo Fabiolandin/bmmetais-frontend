e3c2ef8bf4644a01891d253e67b24d13c37b3229
Fabio Landin <flandin1990@gmail.com>
2026-04-30 09:54:30 -0300

    feat: adicionando paginação nos fetchs de produto e categoria de produto

--- src/fetchs/fetchCategoria.tsx
+++ src/fetchs/fetchCategoria.tsx
@@ -1,5 +1,5 @@
-export const fetchCategoria = async () => {
-    const res = await fetch("http://localhost:3000/categoria", {
+export const fetchCategoria = async (page: number = 1, limit: number = 7) => {
+    const res = await fetch(`http://localhost:3000/categoria?page=${page}&limit=${limit}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -8,7 +8,7 @@ export const fetchCategoria = async () => {
     if (res.status !== 200 && res.status !== 201) {
         throw new Error("Erro ao buscar categoria")
     }
-    return await res.json()
+    return await res.json() as { data: any[]; total: number; page: number; totalPages: number }
 };
 
 export const createCategoria = async (nome: string) => {
--- src/fetchs/fetchProduto.tsx
+++ src/fetchs/fetchProduto.tsx
@@ -1,5 +1,5 @@
-export const fetchProduto = async () => {
-    const res = await fetch("http://localhost:3000/produtos", {
+export const fetchProduto = async (page: number = 1, limit: number = 7) => {
+    const res = await fetch(`http://localhost:3000/produtos?page=${page}&limit=${limit}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -8,7 +8,7 @@ export const fetchProduto = async () => {
     if (res.status !== 200 && res.status !== 201) {
         throw new Error("Erro ao buscar produto")
     }
-    return await res.json()
+    return await res.json() as { data: any[]; total: number; page: number; totalPages: number }
 };
 
 export const createProduto = async (nome: string, descricao: string, preco: number, categoria_produtoId: number, estoque: number,) => {
@@ -33,7 +33,7 @@ export const deleteProduto = async (id: number) => {
             "Content-Type": "application/json",
         },
     })
-    
+
     console.log("Status da resposta de exclusão:", res.status)
 
     if (res.status === 204) return null
