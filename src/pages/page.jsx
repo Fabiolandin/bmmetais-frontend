da8015b10b6404cbb5670ae54b4d349935a13b49
Fabio Landin <flandin1990@gmail.com>
2026-07-14 14:49:31 -0300

    feat: atualizando dash para grafico recharts com pedidosgrafico

--- src/pages/page.jsx
+++ src/pages/page.jsx
@@ -1,7 +1,18 @@
 import Sidebar from "@/components/Sidebar"
 import { Card, CardContent } from "@/components/ui/card"
 import { fetchDashboard } from "@/fetchs/fetchDashboard"
+import { fetchPedidosGrafico } from "@/fetchs/fetchPedidosGrafico";
 import { useEffect, useState } from "react"
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
 
 const Main = () => {
 
@@ -10,17 +21,25 @@ const Main = () => {
     const [tCompras, settCompras] = useState()
     const [tProdutos, settProdutos] = useState()
 
+    //states para receber os dados dos graficos
+    const [graficoDados, setgraficoDados] = useState()
+
     const getDados = async () => {
         const lista = await fetchDashboard()
+        const grafico = await fetchPedidosGrafico()
         settPedidos(lista.totalPedidos)
         settCompras(lista.totalCompras)
         settProdutos(lista.totalProdutos)
+        setgraficoDados(grafico)
     }
 
     useEffect(() => {
         getDados()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    
+
+    console.log(graficoDados)
+
     return (
         <div className="flex">
             <Sidebar />
@@ -55,6 +74,37 @@ const Main = () => {
                     </Card>
 
                 </div>
+
+                <ResponsiveContainer
+                    width="95%"
+                    height={450}
+                    className="p-2"
+                >
+
+                {/* gráfico de pedidos */}
+                <LineChart data={graficoDados} className="mt-10">
+                    <CartesianGrid strokeDasharray="3 3" />
+
+                    {/* eixo Horizontal */}
+                    <XAxis dataKey="mes" />
+
+                    {/* eixo Vertical */}
+                    <YAxis />
+
+                    <Tooltip />
+
+                    <Line
+                        type="linear"
+                        dataKey="total_pedidos"
+                        stroke="red"
+                    />
+                    <Line
+                        dataKey="total_faturamento"
+                        stroke="#26a641"
+                    />
+                </LineChart>
+                </ResponsiveContainer>
+
             </div>
         </div>
     )
