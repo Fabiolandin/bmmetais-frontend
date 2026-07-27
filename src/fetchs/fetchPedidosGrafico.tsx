ae61283eca0b4c012b4bc22f579a8afb2073508c
Fabio Landin <flandin1990@gmail.com>
2026-07-27 16:31:19 -0300

    feat: centralizando fetch PedidosGrafico no apiclient

--- src/fetchs/fetchPedidosGrafico.tsx
+++ src/fetchs/fetchPedidosGrafico.tsx
@@ -1,12 +1,8 @@
+import { apiClient } from "../api/apiClient";
+
 export const fetchPedidosGrafico = async () => {
-    const res = await fetch(`http://localhost:3000/pedidosgrafico`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao buscar pedido")
-    }
-    return await res.json() as { mes: string; total_pedidos: number; total_faturamento: number;}
+    return apiClient< { mes: string; total_pedidos: number; total_faturamento: number;} >(
+        `/pedidosgrafico`
+    )
 };
+
