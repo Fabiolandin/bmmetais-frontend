4b43c22c9337855bc22fdfea9d6adb5d1dee53a4
Fabio Landin <flandin1990@gmail.com>
2026-07-27 16:31:02 -0300

    feat: centralizando fetch Pedido no apiclient

--- src/fetchs/fetchPedido.tsx
+++ src/fetchs/fetchPedido.tsx
@@ -1,40 +1,19 @@
+import { apiClient } from "../api/apiClient";
+
 export const fetchPedido = async (page: number = 1, limit: number = 7) => {
-    const res = await fetch(`http://localhost:3000/pedido?page=${page}&limit=${limit}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao buscar pedido")
-    }
-    return await res.json() as { data: any[]; page: number; total: number; totalPages: number }
+    return apiClient<{ data: any[]; page: number; total: number; totalPages: number }>(
+        `/pedido?page=${page}&limit=${limit}`
+    )
 };
 
 export const createPedido = async (data: { clienteId: number, funcionarioId: number, items: { produtoId: number, quantidade: number, preco_unitario: number }[] }) => {
-    const res = await fetch("http://localhost:3000/pedido", {
+    return apiClient(`/pedido`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao criar pedido")
-    }
-    return await res.json()
 };
 
 export const deletePedido = async (id: number) => {
-    const res = await fetch(`http://localhost:3000/pedido/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201 && res.status !== 204) {
-        throw new Error("Erro ao deletar pedido")
-    }
-    return res.status === 204 ? null : await res.json()
+    return apiClient(`/pedido/${id}`, {method: "DELETE"})
 };
 
