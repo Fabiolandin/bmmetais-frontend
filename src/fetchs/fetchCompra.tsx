04901f85567619341b2604b9a02c687dcb00cf18
Fabio Landin <flandin1990@gmail.com>
2026-07-28 10:41:46 -0300

    feat: centralizando fetch Compra no apiclient

--- src/fetchs/fetchCompra.tsx
+++ src/fetchs/fetchCompra.tsx
@@ -1,39 +1,18 @@
+import { apiClient } from "../api/apiClient";
+
 export const fetchCompras = async (page: number = 1, limit: number = 7) => {
-    const res = await fetch(`http://localhost:3000/compra?page=${page}&limit=${limit}`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao buscar compra")
-    }
-    return await res.json() as { data: any[]; total: number; page: number; totalPages: number }
+    return apiClient<{ data: any[]; total: number; page: number; totalPages: number }>(
+        `/compra?page=${page}&limit=${limit}`
+    )
 };
 
 export const createCompra = async (data: { fornecedorId: number, funcionarioId: number, items: { produtoId: number, quantidade: number, preco_unitario: number }[] }) => {
-    const res = await fetch('http://localhost:3000/compra', {
+    return apiClient(`/compra/`, {
         method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao criar compra")
-    }
-    return await res.json()
 };
 
 export const deleteCompra = async (id: number) => {
-    const res = await fetch(`http://localhost:3000/compra/${id}`, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao deletar compra")
-    }
-    return await res.json()
+    return apiClient(`/compra/${id}`, {method: "DELETE" })
 };
