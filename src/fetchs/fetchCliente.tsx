b94623711d89949f1ca12d2ca303cad24cbf58d2
Fabio Landin <flandin1990@gmail.com>
2026-07-28 10:41:38 -0300

    feat: centralizando fetch Cliente no apiclient

--- src/fetchs/fetchCliente.tsx
+++ src/fetchs/fetchCliente.tsx
@@ -1,53 +1,25 @@
+import { apiClient } from "../api/apiClient";
+
 export const fetchCliente = async (page: number = 1, limit: number = 7) => {
-    const res = await fetch(`http://localhost:3000/cliente?page=${page}&limit=${limit}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao buscar cliente")
-    }
-    return await res.json() as { data: any[]; total: number; page: number; totalPages: number }
+    return apiClient<{ data: any[]; total: number; page: number; totalPages: number }>(
+        `/cliente?page=${page}&limit=${limit}`
+    )
 };
 
 export const createCliente = async (nome: string, cpf: string, telefone: string, email: string) => {
-    const res = await fetch("http://localhost:3000/cliente", {
+    return apiClient(`/cliente/`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome, cpf, telefone, email }),
+        body: JSON.stringify({ nome, cpf, telefone, email })
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao criar cliente")
-    }
-    return await res.json()
 };
 
 export const deleteClientes = async (id: number) => {
-    const res = await fetch(`http://localhost:3000/cliente/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao deletar cliente")
-    }
-    return await res.json()
+    return apiClient(`/cliente/${id}`, {method: "DELETE"})
 };
 
 export const editarCliente = async (id: number, nome: string, cpf: string, telefone: string, email: string) => {
-    const res = await fetch(`http://localhost:3000/cliente/${id}`, {
+    return apiClient(`/cliente/${id}`, {
         method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome, cpf, telefone, email }),
+        body: JSON.stringify({nome, cpf, telefone, email})
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao editar cliente")
-    }
-    return await res.json()
 };
