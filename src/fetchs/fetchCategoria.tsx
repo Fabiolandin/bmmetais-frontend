5e7b4bc557162b5621deee7f02189da045d3c8f0
Fabio Landin <flandin1990@gmail.com>
2026-07-28 10:41:29 -0300

    feat: centralizando fetch Categoria no apiclient

--- src/fetchs/fetchCategoria.tsx
+++ src/fetchs/fetchCategoria.tsx
@@ -1,53 +1,25 @@
+import { apiClient } from "../api/apiClient";
+
 export const fetchCategoria = async (page: number = 1, limit: number = 7) => {
-    const res = await fetch(`http://localhost:3000/categoria?page=${page}&limit=${limit}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao buscar categoria")
-    }
-    return await res.json() as { data: any[]; total: number; page: number; totalPages: number }
+    return apiClient<{ data: any[]; total: number; page: number; totalPages: number }>(
+        `/categoria?=page${page}&limit=${limit}`
+    )
 };
 
 export const createCategoria = async (nome: string) => {
-    const res = await fetch("http://localhost:3000/categoria", {
+    return apiClient(`/categoria/`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome }),
+        body: JSON.stringify({nome})
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao criar categoria")
-    }
-    return await res.json()
 };
 
 export const deleteCategoria = async (id: number) => {
-    const res = await fetch(`http://localhost:3000/categoria/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao deletar categoria")
-    }
-    return await res.json()
+    return apiClient(`/categoria/${id}`, {method: "DELETE"})
 };
 
 export const editarCategoria = async (id: number, nome: string) => {
-    const res = await fetch(`http://localhost:3000/categoria/${id}`, {
+    return apiClient(`/categoria/${id}`, {
         method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ nome }),
+        body: JSON.stringify({ nome })
     })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao atualizar categoria")
-    }
-    return await res.json()
 };
