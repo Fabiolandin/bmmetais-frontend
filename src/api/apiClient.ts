ba8dba4690c33d0db84b061127d13e4ff371640f
Fabio Landin <flandin1990@gmail.com>
2026-08-10 13:58:20 -0300

    fix: adicionando validação que força o reload caso o token seja invalido

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -9,14 +9,23 @@ export async function apiClient<T>(
     options: RequestInit = {}
 
 ): Promise<T> {
+    const token = localStorage.getItem("token");
+
     const res = await fetch(`${API_URL}${endpoint}`, {
         ...options,
         //Header padrão dos fetchs
         headers: {
             "Content-Type": "application/json",
+            ...(token ? { Authorization: `Bearer ${token}` } : {}),
             ...options.headers,
         },
     });
+
+    if (res.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+        throw new Error("Sessão expirada");
+      }
 
     if (!res.ok) {
         const errorText = await res.text();
