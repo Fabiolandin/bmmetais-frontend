552d74e281330362f73f601bad900bfc1cca4a52
Fabio Landin <flandin1990@gmail.com>
2026-08-10 14:00:53 -0300

    fix: arrumando bug de apiClient dentro do dashboard

--- src/fetchs/fetchDashboard.tsx
+++ src/fetchs/fetchDashboard.tsx
@@ -1,3 +1,5 @@
+import { apiClient } from "@/api/apiClient"
+
 export interface DashboardData {
     totalPedidos: number
     totalCompras: number
@@ -5,14 +7,5 @@ export interface DashboardData {
 }
 
 export const fetchDashboard = async (): Promise<DashboardData> => {
-    const res = await fetch(`http://localhost:3000/dashboard`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    if (res.status !== 200 && res.status !== 201) {
-        throw new Error("Erro ao criar pedido")
-    }
-    return await res.json() as DashboardData
+    return apiClient<DashboardData>(`/dashboard`)
 }
